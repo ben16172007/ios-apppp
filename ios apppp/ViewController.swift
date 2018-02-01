b7c2a95b2d498595a93b1e9845386de4f77f372d
john <john@johns-MacBook-Pro.local>
2018-02-01 17:13:47 +0800

    Initial Commit

---
+++ ios apppp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios apppp
+//
+//  Created by john on 2018/2/1.
+//  Copyright © 2018年 roger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
